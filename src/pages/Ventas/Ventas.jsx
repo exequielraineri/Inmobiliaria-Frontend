bf1f6d560a6faa91e901b0f4a24bec1f870ac405
Exequiel Raineri <exeraineri15@gmail.com>
2024-10-31 16:54:42 -0300

    fix vercel

--- src/pages/Ventas/Ventas.jsx
+++ src/pages/Ventas/Ventas.jsx
@@ -1,11 +1,3 @@
-import React from "react";
-
 export const Ventas = () => {
-  return (
-    <div>
-      <div>
-        <table></table>
-      </div>
-    </div>
-  );
+  return <div>Ventas</div>;
 };
