7e22418c146da1f3a2809ad10f0a808d1e33d149
Exequiel Raineri <exeraineri15@gmail.com>
2024-10-31 16:52:51 -0300

    fix vercel

--- src/Routes/RoutesComponent.jsx
+++ src/Routes/RoutesComponent.jsx
@@ -2,7 +2,6 @@ import { Route, Routes } from "react-router-dom";
 import { Inicio } from "../pages/Inicio/Inicio";
 import { Inmuebles } from "../pages/Inmuebles/Inmuebles";
 import { Clientes } from "../pages/Clientes/Clientes";
-import { Usuarios } from "../pages/Usuarios/Usuarios";
 import { Alquiler } from "../pages/Alquiler/Alquiler";
 import { Ventas } from "../pages/Ventas/Ventas";
 import { Agentes } from "../pages/Agentes/Agentes";
@@ -10,7 +9,7 @@ import { Consultas } from "../pages/Consultas/Consultas";
 import { Contratos } from "../pages/Contratos/Contratos";
 import { FormularioIngresoInmueble } from "../pages/Inmuebles/FormularioIngresoInmueble";
 import { FormAlquiler } from "../pages/Alquiler/FormAlquiler";
-import { VerAlquiler } from "../pages/Alquiler/verAlquiler";
+import { VerAlquiler } from "../pages/Alquiler/VerAlquiler";
 
 export const RoutesComponent = () => {
   return (
